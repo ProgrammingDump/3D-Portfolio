ad646251442b0d695736bbaa18c22e4171df760b
ProgrammingDump <sherdilk12@gmail.com>
2024-02-06 03:01:38 +0500

    Hero Section, scroll pending

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import CanvasLoader from "../Loader";
 
 const Computers = () => {
+  const computer = useGLTF("./desktop_pc/scene.gltf");
   return (
-    <div>Computers</div>
-  )
-}
+    <group>
+      <hemisphereLight intensity={2.5} groundColor="black" />
+      <pointLight intensity={2.5} />
+      <spotLight
+      position={[-20, 50, 10]}
+      angle={0.12}
+      penumbra={1}
+      castShadow
+      shadow-mapSize={1024}
+      />
+      <primitive 
+      object={computer.scene}
+      scale={0.85}
+      position={[0, -3.25, -0]} />
+    </group>
+  );
+};
 
-export default Computers
+const ComputersCanvas = () => {
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default ComputersCanvas;
