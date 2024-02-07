b619abbe6175ca234aee1f9cff9bfcc2af5651df
ProgrammingDump <sherdilk12@gmail.com>
2024-02-07 16:55:22 +0500

    progress in tech section

--- src/constants/index.js
+++ src/constants/index.js
@@ -125,24 +125,11 @@ const experiences = [
     ],
   },
   {
-    title: 'React Native Developer',
-    company_name: 'Tesla',
-    icon: tesla,
-    iconBg: '#E6DEDD',
-    date: 'Jan 2021 - Feb 2022',
-    points: [
-      'Developing and maintaining web applications using React.js and other related technologies.',
-      'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
-      'Implementing responsive design and ensuring cross-browser compatibility.',
-      'Participating in code reviews and providing constructive feedback to other developers.',
-    ],
-  },
-  {
     title: 'Web Developer',
     company_name: 'Shopify',
     icon: shopify,
     iconBg: '#383E56',
-    date: 'Jan 2022 - Jan 2023',
+    date: 'Jan 2021 - Jan 2022',
     points: [
       'Developing and maintaining web applications using React.js and other related technologies.',
       'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
@@ -151,9 +138,22 @@ const experiences = [
     ],
   },
   {
-    title: 'Full stack Developer',
-    company_name: 'Meta',
-    icon: meta,
+    title: 'React Native Developer',
+    company_name: 'Tesla',
+    icon: tesla,
+    iconBg: '#E6DEDD',
+    date: 'Feb 2022 - Feb 2023',
+    points: [
+      'Developing and maintaining web applications using React.js and other related technologies.',
+      'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
+      'Implementing responsive design and ensuring cross-browser compatibility.',
+      'Participating in code reviews and providing constructive feedback to other developers.',
+    ],
+  },
+  {
+    title: 'Full Stack Developer',
+    company_name: 'MERN',
+    icon: mongodb,
     iconBg: '#E6DEDD',
     date: 'Jan 2023 - Present',
     points: [
